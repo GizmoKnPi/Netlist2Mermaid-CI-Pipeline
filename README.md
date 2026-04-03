57eb00990e85886f3ead3163735b630ead773431
GizmoKnPi <kiranpunarthum@gmail.com>
2026-04-03 22:57:54 +0530

    netlist upated

--- README.md
+++ README.md
@@ -67,14 +67,14 @@ graph LR;
     stage1_mux1["stage1_mux1<br><b>mux4_1</b>"]:::DEFAULT_MOD
     data_in_3_0_(["data_in[3:0]"]) -->|i| stage1_mux1;
     sel_1_0_(["sel[1:0]"]) -->|s| stage1_mux1;
-    stage1_mux1 -->|y| Y01(["Y01"]);
+    stage1_mux1 -->|y| y1(["y1"]);
     stage1_mux2["stage1_mux2<br><b>mux4_1</b>"]:::DEFAULT_MOD
     data_in_7_4_(["data_in[7:4]"]) -->|i| stage1_mux2;
     sel_1_0_(["sel[1:0]"]) -->|s| stage1_mux2;
-    stage1_mux2 -->|y| Y02(["Y02"]);
+    stage1_mux2 -->|y| y2(["y2"]);
     stage2_mux["stage2_mux<br><b>mux2_1</b>"]:::DEFAULT_MOD
-    Y01(["Y01"]) -->|i0| stage2_mux;
-    Y02(["Y02"]) -->|i1| stage2_mux;
+    y1(["y1"]) -->|i0| stage2_mux;
+    y2(["y2"]) -->|i1| stage2_mux;
     sel_2_(["sel[2]"]) -->|s| stage2_mux;
     stage2_mux -->|y| y(["y"]);
 ```
--- netlist-visulaizer/8_1_mux.v
+++ netlist-visulaizer/8_1_mux.v
@@ -4,18 +4,18 @@ module mux8_1_structural(
     output y             // Output
 );
     // Declare intemediate wires to connect the submodules
-    wire Y01, Y02;
+    wire y1, y2;
 
     // Instantiate two 4:1 MUXs for the first stage
-    // The outputs of the first stage are Y01 and Y02
+    // The outputs of the first stage are y1 and y2
     // The inputs are split into two 4-bit groups
     // The select lines s[1:0] (S1 and S0) are shared
-    mux4_1 stage1_mux1 (.i(data_in[3:0]), .s(sel[1:0]), .y(Y01));
-    mux4_1 stage1_mux2 (.i(data_in[7:4]), .s(sel[1:0]), .y(Y02));
+    mux4_1 stage1_mux1 (.i(data_in[3:0]), .s(sel[1:0]), .y(y1));
+    mux4_1 stage1_mux2 (.i(data_in[7:4]), .s(sel[1:0]), .y(y2));
 
     // Instantiate one 2:1 MUX for the second stage
-    // The inputs are the outputs from the first stage (Y01, Y02)
+    // The inputs are the outputs from the first stage (y1, y2)
     // The select line s[2] (S2) controls which 4:1 MUX's output is chosen
-    mux2_1 stage2_mux (.i0(Y01), .i1(Y02), .s(sel[2]), .y(y));
+    mux2_1 stage2_mux (.i0(y1), .i1(y2), .s(sel[2]), .y(y));
 
 endmodule
